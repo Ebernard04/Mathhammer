e55e272d6ea181ab7fe9c0a2842540209f3abc52
Eric Bernard <infinitebomb@gmail.com>
2025-02-10 15:18:06 -0600

    creation/update of master use function for weapons

--- Sourcecode/weapon.py
+++ Sourcecode/weapon.py
@@ -10,60 +10,8 @@ class weapon:
         self.strength = strength
         self.penetration = penetration
         self.damage = damage
-
-    def basic_hit(self, modifier):
-        roll = random.randint(1,6)
-        if(modifier != 0):
-            roll += modifier
-        return (roll >= self.skill)
-
-    
-    def wound(self, modifier, toughness):
-        roll = random.randint(1,6)
-        if(modifier != 0):
-            roll += modifier
-        check = 4
-        if(self.strength == toughness):
-            check = 4
-        else:
-            if(self.strength > toughness):
-                if(self.strength > (2*toughness)):
-                    check = 2
-                else:
-                    check = 3
-            else:
-                if(self.strength < toughness):
-                    if((self.strength*2) < toughness):
-                        check = 6
-                    else:
-                        check = 5
-        return (roll>=check)
-    
-    def use(self, hit_modifier, wound_modifier, toughness, reroll_hit = False, reroll_wound = False):
-        numhits = 0
-        numwounds = 0
-        for x in range(self.attacks):
-            hval = self.hit(self, hit_modifier)
-            if (hval):
-                numhits+=1
-            elif (reroll_hit):
-                hval = self.hit(self, hit_modifier)
-                if (hval):
-                    numhits+=1
-        if(numhits==0):
-            return 0
-        else:
-            for y in range(numhits):
-                wval = self.wound(self,wound_modifier,toughness)
-                if(wval):
-                    numwounds+=1
-                elif(reroll_wound):
-                    wval=self.wound(self,wound_modifier,toughness)
-                    if(wval):
-                        numwounds+=1
-        return numwounds
                     
-    def roll_out_hits(self, modifier, reroll_ones = False, reroll_all = False, sustained = False, lethal = False, fish = False, crit = 6):
+    def hit_roll(self, modifier, reroll_ones = False, reroll_all = False, sustained = False, lethal = False, fish = False, crit = 6):
         raw = np.zeros(self.attacks, dtype = np.int64)
         for x in range(raw.size):
             raw[x] = random.randint(1,6)
@@ -111,7 +59,7 @@ class weapon:
             results[1]=num_leth
         return results
     
-    def roll_out_wounds(self, toughness, modifier, reroll_ones = False, reroll_all = False, devastating = False, fish = False, hits = 0, crit = 6):
+    def wound_roll(self, toughness, modifier, reroll_ones = False, reroll_all = False, devastating = False, fish = False, hits = 0, crit = 6):
         raw = np.zeros(hits, dtype = np.int64)
         if(hits==0):
             return 0
@@ -170,5 +118,12 @@ class weapon:
             results[1] = num_dev
         return results
         
-
+    def use(self, hit_modifier, wound_modifier, toughness, reroll_hit = False, reroll_wound = False, reroll_hit_ones = False, reroll_wound_ones = False, fish_hits = False, fish_wounds = False, crit = 6, sustained = False, lethal = False, devastating = False):
+        hit_data = self.hit_roll(hit_modifier ,reroll_hit_ones, reroll_hit, sustained, lethal, fish_hits, crit) ##returns array of [hits, lethals]
+        wound_data = self.wound_roll(toughness,wound_modifier,reroll_wound_ones,reroll_wound,devastating,fish_wounds,hit_data[0],crit) ## returns array of [wounds, dev wounds]
+        if(lethal):
+            wound_data[0]+=hit_data[1]
+        atk_data = np.array([self.penetration,self.damage])
+        output = np.append(wound_data,atk_data) ## array should have format [wounds, dev wounds, pen value, damage per wound]
+        return output
 
