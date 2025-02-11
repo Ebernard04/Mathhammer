f6a9e21aa90e37079ceca25ed207d22fbef0dfc0
Eric Bernard <infinitebomb@gmail.com>
2025-02-11 14:20:44 -0600

    added two default weapons to for eventual testing

---
+++ Sourcecode/bolt_pistol.py
@@ -0,0 +1,27 @@
+import weapon
+
+class bolt_pistol(weapon):
+    def __init__(self):
+        weapon_attributes = {"assault": False,
+                            "rapid fire": False,
+                            "ignores cover": False,
+                            "twin-linked": False,
+                            "pistol": True,
+                            "torrent": False,
+                            "lethal": False,
+                            "lance": False,
+                            "indirect fire": False,
+                            "precision": False,
+                            "blast": False,
+                            "heavy": False,
+                            "hazardous": False,
+                            "devastating": False,
+                            "sustained": False,
+                            "extra attacks": False,
+                            "anti-infantry": False,
+                            "anti-monster": False,
+                            "anti-vehicle": False,
+                            "anti-mounted": False,
+                            "anti-psyker": False
+                        }
+        super.__init__(self, 12, 1, 3, 4, 0, 1,weapon_attributes,0,0)
---
+++ Sourcecode/close_combat_weapon.py
@@ -0,0 +1,28 @@
+import weapon
+
+class close_combat_weapon(weapon):
+    def __init__(self):
+        weapon_attributes = {
+                            "assault": False,
+                            "rapid fire": False,
+                            "ignores cover": False,
+                            "twin-linked": False,
+                            "pistol": False,
+                            "torrent": False,
+                            "lethal": False,
+                            "lance": False,
+                            "indirect fire": False,
+                            "precision": False,
+                            "blast": False,
+                            "heavy": False,
+                            "hazardous": False,
+                            "devastating": False,
+                            "sustained": False,
+                            "extra attacks": False,
+                            "anti-infantry": False,
+                            "anti-monster": False,
+                            "anti-vehicle": False,
+                            "anti-mounted": False,
+                            "anti-psyker": False
+                        }
+        super.__init__(1,3,3,4,0,1,weapon_attributes,0,0)
